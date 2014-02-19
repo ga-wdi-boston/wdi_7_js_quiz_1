a20f0d38588e1d7f256713a674df460e15d735a5
Janice Smith <janice.k.n.smith@gmail.com>
2014-02-19 10:21:45 -0500

    got 3 of them

--- src/quiz.js
+++ src/quiz.js
@@ -4,15 +4,24 @@
 //      |  |     |   __   | |  |     \   \       |  |     \   \         /  /_\  \      |  |  |  |   |  |  |  | |  |    /  /
 //      |  |     |  |  |  | |  | .----)   |      |  | .----)   |       /  _____  \     |  `--'  '--.|  `--'  | |  |   /  /----.__
 //      |__|     |__|  |__| |__| |_______/       |__| |_______/       /__/     \__\     \_____\_____\\______/  |__|  /________(__)
-var quiz = {};
+
 
 // Question 1: a function called sleepIn
 //////////////
 // You can sleep in if it is not a weekday or if you are on vacation.
 // quiz.sleepIn() => false
 // quiz.sleepIn({vacation: true}) => true
-quiz.sleepIn = function(options) {
-}
+var quiz = {};
+
+quiz.sleepIn = function(options){
+	var today=new Date(),
+	    thisDay=today.getDay();
+	if(vacation === true || thisDay !== 0 || thisDay !== 6 ){
+		console.log("sleep in!");
+	} else {
+		console.log("no sleeping");
+	}
+};
 
 // Question 2: function called nearHundred
 //////////////
@@ -22,6 +31,14 @@ quiz.sleepIn = function(options) {
 // quiz.nearHundred(93) => true
 // quiz.nearHundred('two') => Error: Please enter a number!
 
+quiz.nearHundred = function(n){
+	if(n >= 90 && n <= 100 ){
+		console.log("near 100");
+	} else {
+		console.log("nope");
+	}
+};
+
 // Question 3: a function called missingChar
 //////////////
 // Remove the character that corresponds to the index from the string.
@@ -29,14 +46,42 @@ quiz.sleepIn = function(options) {
 // quiz.missingChar("kittie", 1) => "kttie"
 // quiz.missingChar(347, 1) => Error: Please enter a string!
 
+quiz.missingChar = function(string, index1, index2){
+	var str2= string.slice(index1, index2),
+	    str3 = string.replace(str2, "")
+	return str3;
+};
+
 // Question 4: a function called delDel
 //////////////
 // Remove "del" from a string.
 // quiz.delDel("abdelcd") => "abcd"
 // quiz.delDel("xyz") => "xyz"
 
+quiz.delDel = function(string){
+	var str2 = string.replace("del","");
+	return str2;
+};
+
 // Question 5: a method called backAround
 //////////////
 // Given a string, move the last character to the beginning.
 // "cat".backAround() => "tca"
 // "hello".backAround() => "ohell"
+
+quiz.backAround = function(string){
+  var str2 = string[-1] + string[0...(string.length - 1)]
+  return str2;
+};
+
+
+
+
+
+
+
+
+
+
+
+
