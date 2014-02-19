f12fbcda79fd586344abd8d23e72eb802da3f3e9
Prescott Murphy <prurph@gmail.com>
2014-02-19 10:18:46 -0500

    Add functions

--- src/quiz.js
+++ src/quiz.js
@@ -12,7 +12,15 @@ var quiz = {};
 // quiz.sleepIn() => false
 // quiz.sleepIn({vacation: true}) => true
 quiz.sleepIn = function(options) {
-}
+  var today = new Date().getDay(),
+      options = options || {};
+      sleep_in = false;
+
+  if (today === 0 || today === 6 || options.vacation) {
+    sleep_in = true;
+  }
+  return sleep_in;
+};
 
 // Question 2: function called nearHundred
 //////////////
@@ -21,6 +29,15 @@ quiz.sleepIn = function(options) {
 // quiz.nearHundred(52) => false
 // quiz.nearHundred(93) => true
 // quiz.nearHundred('two') => Error: Please enter a number!
+quiz.nearHundred = function(num) {
+  if (typeof num !== "number") {
+    throw "Error: please enter a number!";
+  } else if (90 <= num && num <= 99) { // 101 is not nearHundred per above :]
+    return true;
+  } else {
+    return false;
+  }
+};
 
 // Question 3: a function called missingChar
 //////////////
@@ -28,15 +45,34 @@ quiz.sleepIn = function(options) {
 // If you don't enter a string
 // quiz.missingChar("kittie", 1) => "kttie"
 // quiz.missingChar(347, 1) => Error: Please enter a string!
+quiz.missingChar = function(str, indx) {
+  if (typeof str !== "string") {
+    throw "Error: Please enter a string!";
+  }
+  return (str.slice(0, indx)+str.slice(indx+1,str.length));
+};
 
 // Question 4: a function called delDel
 //////////////
 // Remove "del" from a string.
 // quiz.delDel("abdelcd") => "abcd"
 // quiz.delDel("xyz") => "xyz"
+quiz.delDel = function(str) {
+  return str.split("del").join(""); // removes all occurrences
+};
 
 // Question 5: a method called backAround
 //////////////
 // Given a string, move the last character to the beginning.
 // "cat".backAround() => "tca"
 // "hello".backAround() => "ohell"
+
+// Make as method on String objects
+String.prototype.backAround = function() {
+  return this[this.length -1] + this.slice(0,-1);
+};
+
+// Or as a standalone in quiz module
+quiz.backAround = function(str) {
+  return str[str.length - 1] + str.slice(0,-1);
+};
